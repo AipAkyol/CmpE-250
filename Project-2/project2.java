b8c372802f07280641630f1f3cd54c91752040dc
Alperen Akyol <a-akyol@hotmail.com>
2023-11-13 21:12:59 +0300

    first commit

---
+++ Project-2/project2.java
@@ -0,0 +1,38 @@
+public class project2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void executeinput(String line) {
+        String[] input = line.split(" ");
+        switch (input[0]) {
+            case "add":
+                break;
+            case "remove":
+                break;
+            case "print":
+                break;
+            case "printall":
+                break;
+            case "quit":
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
+    }
+
+    public class hashTable {
+        
+
+    
+        
+    }
+
+    public class employee {
+        private String name;
+        private String profession;
+        private int score;
+    }
+}
