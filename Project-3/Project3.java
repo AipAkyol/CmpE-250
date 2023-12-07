ea0f392ffca74eb9748d16a205be1ab67e968811
Alperen Akyol <a-akyol@hotmail.com>
2023-12-08 00:17:43 +0300

    radix

--- Project-3/Project3.java
+++ Project-3/Project3.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 import java.io.FileNotFoundException;
@@ -7,7 +9,7 @@ import java.io.FileNotFoundException;
 public class Project3 {
     public static void main(String[] args) throws FileNotFoundException {
         
-        File songFile = new File("C:\\BOUN\\CmpE250\\CmpE-250\\Project-3\\sample-test-cases\\songs.txt");
+        File songFile = new File("C:\\BOUN\\CmpE250\\CmpE-250\\Project-3\\test-cases\\songs.txt");
         Scanner songScanner = new Scanner(songFile);
         
         Song[] songDatabase = new Song[Integer.parseInt(songScanner.nextLine()) + 1];
@@ -21,16 +23,22 @@ public class Project3 {
             for (int i = 0; i < 3; i++) {
                 scores[i] = Integer.parseInt(songInfo[3 + i]);
             }
-            songDatabase[id] = new Song(id, name, playCount, scores);
+            songDatabase[id] = new Song(id, name, playCount, scores, false);
         }
-
+        songDatabase[0] = new Song(0, "aaaaa", 0, new int[]{0, 0, 0}, false); // dummy node
         songScanner.close();
+        long initialTime = System.currentTimeMillis();
+        MergeSort.sortSongs(songDatabase);
+        long finalTime = System.currentTimeMillis();
+        System.out.println("Merge Sort: " + (finalTime - initialTime) + " ms");
 
+        /* 
         // TODO: check starting sizes
         MaxHeap heartacheHeap = new MaxHeap(100, 0);
         MaxHeap roadtripHeap = new MaxHeap(100, 1);
         MaxHeap blissfulHeap = new MaxHeap(100, 2);
-
+        */
+        /* 
         File inputFile = new File("C:\\BOUN\\CmpE250\\CmpE-250\\Project-3\\sample-test-cases\\inputs\\sample_0.txt");
         Scanner inputScanner = new Scanner(inputFile);
 
@@ -40,22 +48,32 @@ public class Project3 {
         final int LIMIT_ROADTRIP = Integer.parseInt(limits[2]);
         final int LIMIT_BLISSFUL = Integer.parseInt(limits[3]);
 
-        int[] playlistContribitionCounts = new int[Integer.parseInt(inputScanner.nextLine()) + 1];
+        final int PLAYLIST_COUNT = Integer.parseInt(inputScanner.nextLine());
+        //int[] playlistContribitionCounts = new int[PLAYLIST_COUNT + 1];
 
+        Playlist[] playlists = new Playlist[PLAYLIST_COUNT + 1];
+
+        
+        
         //TODO: playlist sizeları dırektolarak atladım
-        for (int i = 1; i < playlistContribitionCounts.length; i++) {
+        for (int i = 1; i < PLAYLIST_COUNT + 1; i++) {
             inputScanner.nextLine();
             // playlistContribitionCounts[i] = 0;
             String[] playlistSongs = inputScanner.nextLine().split(" ");
             for (int j = 0; j < playlistSongs.length; j++) {
                 int songId = Integer.parseInt(playlistSongs[j]);
                 songDatabase[songId].playlistId = i;
+                playlists[i].notInBlendHeartache.insert(songDatabase[songId]);
+                playlists[i].notInBlendRoadtrip.insert(songDatabase[songId]);
+                playlists[i].notInBlendBlissful.insert(songDatabase[songId]);
                 heartacheHeap.insert(songDatabase[songId]);
                 roadtripHeap.insert(songDatabase[songId]);
                 blissfulHeap.insert(songDatabase[songId]);
             }
         }
+        */
 
+        /* 
         //build the initial epic blend
         EpicBlend epicBlend = new EpicBlend();
         
@@ -71,20 +89,20 @@ public class Project3 {
             epicBlend.minBlissful += song.scores[2];
             playlistContribitionCounts[song.playlistId]++;
         }
-
+        */
 
         System.out.println("Heartache Heap");
 
         
-         
+        /*  
         // test if sort runs well in more than 2 duplicates
         Song[] test = new Song[6];
-        test[0] = new Song(0, "a", 0, new int[]{0, 0, 0}, 0);
-        test[1] = new Song(1, "a", 0, new int[]{0, 0, 0}, 0);
-        test[2] = new Song(2, "d", 2, new int[]{0, 0, 0}, 0);
-        test[3] = new Song(3, "c", 0, new int[]{0, 0, 0}, 0);
-        test[4] = new Song(4, "a", 0, new int[]{0, 0, 0}, 0);
-        test[5] = new Song(5, "a", 0, new int[]{0, 0, 0}, 0);
+        test[0] = new Song(0, "a", 0, new int[]{0, 0, 0}, 0, false);
+        test[1] = new Song(1, "a", 0, new int[]{0, 0, 0}, 0, false);
+        test[2] = new Song(2, "d", 2, new int[]{0, 0, 0}, 0, false);
+        test[3] = new Song(3, "c", 0, new int[]{0, 0, 0}, 0, false);
+        test[4] = new Song(4, "a", 0, new int[]{0, 0, 0}, 0, false);
+        test[5] = new Song(5, "a", 0, new int[]{0, 0, 0}, 0, false);
         MergeSort.sort(test);
         for (int i = 0; i < test.length; i++) {
             if (i < test.length - 1) {
@@ -94,7 +112,7 @@ public class Project3 {
             }
             System.out.println(test[i].name);
         }
-        
+        */
 
 
         
@@ -129,22 +147,14 @@ public class Project3 {
     }
 
     public static class EpicBlend {
-        Hashtable blend;
-        int heartacheCount;
-        int roadtripCount;
-        int blissfulCount;
-        int minHeartache;
-        int minRoadtrip;
-        int minBlissful;
+        MinHeap heartacheSongs;
+        MinHeap roadtripSongs;
+        MinHeap blissfulSongs;
 
         public EpicBlend(){
-            this.blend = new Hashtable();
-            this.heartacheCount = 0;
-            this.roadtripCount = 0;
-            this.blissfulCount = 0;
-            this.minHeartache = 0;
-            this.minRoadtrip = 0;
-            this.minBlissful = 0;
+            this.heartacheSongs = new MinHeap(100, 0);
+            this.roadtripSongs = new MinHeap(100, 1);
+            this.blissfulSongs = new MinHeap(100, 2);
         }
             
     }
@@ -199,8 +209,142 @@ public class Project3 {
             }
         }
 
+        //chech namehash in case of equal playcounts
+        public static void radixSort(Song[] array){
+            // First sort by nameHash using binary radix sort
+            binaryRadixSort(array, 0, array.length - 1, 0);
+            // Then perform counting sort for each digit. As max is 10000, we limit to 5 passes.
+            for(int exp = 1; exp <= 10000; exp *= 10){
+                countSort(array, exp);
+            }
+        }
+        
+        public static void countSort(Song[] array, int exp){
+            Song[] output = new Song[array.length];
+            int[] count = new int[10];
+            Arrays.fill(count, 0);
+            for(int i = 0; i < array.length; i++){
+                count[(array[i].playCount / exp) % 10]++;
+            }
+            for(int i = 1; i < 10; i++){
+                count[i] += count[i - 1];
+            }
+            for(int i = array.length - 1; i >= 0; i--){
+                output[--count[(array[i].playCount / exp) % 10]] = array[i];
+            }
+            for(int i = 0; i < array.length; i++){
+                array[i] = output[i];
+            }
+        }
+
+        public static void binaryRadixSort(Song[] array, int left, int right, int bit){
+            if(right <= left || bit < 0){
+                return;
+            }
+            int i = left, j = right;
+            while(i != j){
+                while((array[i].nameHash & (1 << bit)) == 0 && i < j){
+                    i++;
+                }
+                while((array[j].nameHash & (1 << bit)) != 0 && i < j){
+                    j--;
+                }
+                Song temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            if((array[i].nameHash & (1 << bit)) == 0){
+                i++;
+            }
+            binaryRadixSort(array, left, i - 1, bit - 1);
+            binaryRadixSort(array, i, right, bit - 1);
+        }
+
+        public static void mergeSortbyName(Song[] array, int low, int high){
+            if(low < high){
+                int mid = (low + high) / 2;
+                mergeSortbyName(array, low, mid);
+                mergeSortbyName(array, mid + 1, high);
+                mergebyName(array, low, mid, high);
+            }
+        }
+
+        public static void mergebyName(Song[] array, int low, int mid, int high){
+            int left = low;
+            int right = mid + 1;
+            int index = low;
+            Song[] temp = new Song[array.length];
+            for(int i = low; i <= high; i++){
+                temp[i] = array[i];
+            }
+            while(left <= mid && right <= high){
+                if(StringComparator.fastStringCompare(temp[left].name, temp[right].name)){
+                    array[index] = temp[left];
+                    left++;
+                }else {
+                    array[index] = temp[right];
+                    right++;
+                } 
+                index++;
+            }
+            while(left <= mid){
+                array[index] = temp[left];
+                left++;
+                index++;
+            }
+            while(right <= high){
+                array[index] = temp[right];
+                right++;
+                index++;
+            }
+        }
+
         public static void sort(Song[] array){
             sort(array, 0, array.length - 1);
+        }
+
+        public static void sortSongs(Song[] array){
+            radixSortByName(array, 5);
+            countingSortByPlayCount(array, 10000);
+            
+        }
+        
+        public static void countingSortByPlayCount(Song[] array, int maxPlayCount){
+            Song[] output = new Song[array.length];
+            int[] count = new int[maxPlayCount + 1];
+            for(Song song : array){
+                count[song.playCount]++;
+            }
+            for(int i = 1; i <= maxPlayCount; i++){
+                count[i] += count[i - 1];
+            }
+            for(int i = array.length - 1; i >= 0; i--){
+                output[--count[array[i].playCount]] = array[i];
+            }
+            System.arraycopy(output, 0, array, 0, array.length);
+        }
+        
+        public static void radixSortByName(Song[] array, int nameLength){
+            for(int exp = nameLength - 1; exp >= 0; exp--){
+                countingSortByName(array, exp);
+            }
+        }
+        
+        public static void countingSortByName(Song[] array, int exp){
+            Song[] output = new Song[array.length];
+            int[] count = new int[27]; // Assuming names are lowercase English letters
+            for(Song song : array){
+                int index = song.name.charAt(exp) - 'a' + 1;
+                count[index]++;
+            }
+            for(int i = 1; i < 27; i++){
+                count[i] += count[i - 1];
+            }
+            for(int i = array.length - 1; i >= 0; i--){
+                int index = array[i].name.charAt(exp) - 'a' + 1;
+                output[--count[index]] = array[i];
+            }
+            System.arraycopy(output, 0, array, 0, array.length);
         }
     }
 
@@ -320,21 +464,21 @@ public class Project3 {
             this.heap = new Song[100];
             this.size = 0;
             this.scoreIndex = 0;
-            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0)); // dummy node
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
         }
 
         public MaxHeap(int size){
             this.heap = new Song[size];
             this.size = 0;
             this.scoreIndex = 0;
-            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0)); // dummy node
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
         }
 
         public MaxHeap(int size, int scoreIndex){
             this.heap = new Song[size];
             this.size = 0;
             this.scoreIndex = scoreIndex;
-            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0)); // dummy node
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
         }
 
         public void insert(Song song){
@@ -405,6 +549,131 @@ public class Project3 {
             return popped;
         }
 
+        public Song peek(){
+            return heap[1];
+        }
+
+    }
+
+    public static class MinHeap {
+        int scoreIndex; // for song scores
+        Song[] heap;
+        int size;
+
+        public MinHeap(){
+            this.heap = new Song[100];
+            this.size = 0;
+            this.scoreIndex = 0;
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
+        }
+
+        public MinHeap(int size){
+            this.heap = new Song[size];
+            this.size = 0;
+            this.scoreIndex = 0;
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
+        }
+
+        public MinHeap(int size, int scoreIndex){
+            this.heap = new Song[size];
+            this.size = 0;
+            this.scoreIndex = scoreIndex;
+            this.insert(new Song(0, "", 0, new int[]{0, 0, 0}, 0, false)); // dummy node
+        }
+
+        public void insert(Song song){
+            if(size == heap.length){
+                Song[] newHeap = new Song[heap.length * 2];
+                for(int i = 0; i < heap.length; i++){
+                    newHeap[i] = heap[i];
+                }
+                heap = newHeap;
+            }
+            heap[size] = song;
+            size++;
+            int current = size - 1;
+            while(current > 1 && heap[current].scores[scoreIndex] < heap[parent(current)].scores[scoreIndex]){
+                swap(current, parent(current));
+                current = parent(current);
+            }
+        }
+
+        public void swap(int a, int b){
+            Song temp = heap[a];
+            heap[a] = heap[b];
+            heap[b] = temp;
+        }
+
+        public int parent(int pos){
+            return pos / 2;
+        }
+
+        public int leftChild(int pos){
+            return 2 * pos;
+        }
+
+        public int rightChild(int pos){
+            return (2 * pos) + 1;
+        }
+
+        public void minHeapify(int pos){
+            if (pos * 2 >= size) { // leaf node
+                return;
+            }
+            int left = leftChild(pos);
+            int right = rightChild(pos);
+            if(right < size){ // Check if right child exists
+                if(heap[pos].scores[scoreIndex] > heap[left].scores[scoreIndex] || heap[pos].scores[scoreIndex] > heap[right].scores[scoreIndex]){
+                    if(heap[left].scores[scoreIndex] < heap[right].scores[scoreIndex]){
+                        swap(pos, left);
+                        minHeapify(left);
+                    }else{
+                        swap(pos, right);
+                        minHeapify(right);
+                    }
+                }
+            }else if(left < size){ // Check if left child exists
+                if(heap[pos].scores[scoreIndex] > heap[left].scores[scoreIndex]){
+                    swap(pos, left);
+                    minHeapify(left);
+                }
+            }
+        }
+
+        public Song remove(){
+            Song popped = heap[1];
+            heap[1] = heap[size - 1];
+            heap[size - 1] = null;
+            size--;
+            minHeapify(1);
+            return popped;
+        }
+
+        public Song peek(){
+            return heap[1];
+        }
+    }
+
+    public static class Playlist {
+        MaxHeap notInBlendHeartache;
+        MinHeap inBlendHeartache;
+        MaxHeap notInBlendRoadtrip;
+        MinHeap inBlendRoadtrip;
+        MaxHeap notInBlendBlissful;
+        MinHeap inBlendBlissful;
+        int songContributionCount;
+
+        public Playlist(){
+            this.notInBlendHeartache = new MaxHeap(100, 0);
+            this.inBlendHeartache = new MinHeap(100, 0);
+            this.notInBlendRoadtrip = new MaxHeap(100, 1);
+            this.inBlendRoadtrip = new MinHeap(100, 1);
+            this.notInBlendBlissful = new MaxHeap(100, 2);
+            this.inBlendBlissful = new MinHeap(100, 2);
+            this.songContributionCount = 0;
+        }
+
+
     }
 
     public static class Song{
@@ -413,28 +682,58 @@ public class Project3 {
         int playCount;
         int[] scores; // [heartache, roadtrip, blissful]
         int playlistId;
+        boolean inBlend;
+        int nameHash;
 
         public Song(){
             this.id = 0;
             this.scores = new int[3];
             this.playlistId = 0;
             this.playCount = 0;
+            this.name = "";
+            this.inBlend = false;
+            this.nameHash = 0;
         }
 
-        public Song(int id, String name, int playCount, int[] scores){
+        public Song(int id, String name, int playCount, int[] scores, boolean inBlend){
             this.id = id;
             this.name = name;
             this.playCount = playCount;
             this.scores = scores;
             this.playlistId = 0;
+            this.inBlend = inBlend;
+            this.nameHash = computeValue(name);
         }
 
-        public Song(int id, String name, int playCount, int[] scores, int playlistId){
+        public Song(int id, String name, int playCount, int[] scores, int playlistId, boolean inBlend){
             this.id = id;
             this.name = name;
             this.playCount = playCount;
             this.scores = scores;
             this.playlistId = playlistId;
+            this.inBlend = inBlend;
+            this.nameHash = computeValue(name);
+        }
+
+        public static int computeValue(String s) {
+            int value = 0;
+            for (char c : s.toCharArray()) {
+                value = value * 26 + (c - 'a' + 1);
+            }
+            return value;
+        }
+
+        
+    }
+
+    public static class StringComparator {
+        public static boolean fastStringCompare(String str1, String str2) {
+            for (int i = 0; i < 5; i++) {
+                if (str1.charAt(i) < str2.charAt(i)) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
